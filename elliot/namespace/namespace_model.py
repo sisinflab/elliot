743da6b0adbdc63df27f76ca2f3ef16aabcfc507
Claudio Pomo <claudio.pomo@poliba.it>
2021-02-24 15:39:57 +0100

    fixing parameters default

--- elliot/namespace/namespace_model.py
+++ elliot/namespace/namespace_model.py
@@ -92,13 +92,12 @@ class NameSpaceModel:
             .get(_performance, self._set_path(self._base_folder_path_config, "../results/{0}/performance/")) \
             .format(self.config[_experiment][_dataset])
 
-        # self.config[_experiment][_paired_ttest] = self.config[_experiment].get(_paired_ttest, False)
         self.config[_experiment][_dataloader] = self.config[_experiment].get(_dataloader, "DataSetLoader")
 
         manage_directories(self.config[_experiment][_recs], self.config[_experiment][_weights],
                            self.config[_experiment][_performance])
 
-        for p in [_data_config, _weights, _recs, _dataset, _top_k, _paired_ttest, _performance, _logger_config,
+        for p in [_data_config, _weights, _recs, _dataset, _top_k, _performance, _logger_config,
                   _log_folder, _dataloader, _splitting, _prefiltering, _evaluation, _external_models_path,
                   _print_triplets, _config_test]:
             if p == _data_config:
@@ -138,11 +137,15 @@ class NameSpaceModel:
                 setattr(self.base_namespace, p, self.config[_experiment][p])
             elif p == _evaluation and self.config[_experiment].get(p, {}):
                 complex_metrics = self.config[_experiment][p].get("complex_metrics", {})
+                paired_ttest = self.config[_experiment][p].get("paired_ttest", {})
+                wilcoxon_test = self.config[_experiment][p].get("wilcoxon_test", {})
                 for complex_metric in complex_metrics:
                     complex_metric.update({k: self._set_path(self._base_folder_path_config,
                                                       v.format(self.config[_experiment][_dataset]))
                                     for k, v in complex_metric.items() if isinstance(v, str)})
                 self.config[_experiment][p]["complex_metrics"] = complex_metrics
+                self.config[_experiment][p]["paired_ttest"] = complex_metrics
+                self.config[_experiment][p]["wilcoxon_test"] = complex_metrics
                 setattr(self.base_namespace, p, SimpleNamespace(**self.config[_experiment][p]))
             elif p == _logger_config and not self.config[_experiment].get(p, False):
                 setattr(self.base_namespace, p, f"{self._base_folder_path_elliot}/config/logger_config.yml")
