fce2d9140e151dfd984c0728470d728803be5566
walter <vitowalteranelli@gmail.com>
2021-05-13 18:01:13 +0200

    minervini's style

--- elliot/dataset/modular_loaders/loaders.py
+++ elliot/dataset/modular_loaders/loaders.py
@@ -2,3 +2,4 @@
 
 from elliot.dataset.modular_loaders.kg.kahfm_style import ChainedKG
 from elliot.dataset.modular_loaders.generic.item_attributes import ItemAttributes
+from elliot.dataset.modular_loaders.kg.minervini_style import KGCompletion
