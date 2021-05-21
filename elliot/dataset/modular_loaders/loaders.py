78d88f19e21578aa455a27aa6f97643b01281c2f
Claudio Pomo <claudio.pomo@poliba.it>
2021-05-21 09:25:29 +0200

    add loader to loaders

--- elliot/dataset/modular_loaders/loaders.py
+++ elliot/dataset/modular_loaders/loaders.py
@@ -3,3 +3,4 @@
 from elliot.dataset.modular_loaders.kg.kahfm_style import ChainedKG
 from elliot.dataset.modular_loaders.generic.item_attributes import ItemAttributes
 from elliot.dataset.modular_loaders.kg.minervini_style import KGCompletion
+from elliot.dataset.modular_loaders.visual.visual_attribute import VisualAttribute
