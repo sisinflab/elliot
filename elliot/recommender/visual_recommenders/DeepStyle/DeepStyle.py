2bff2885b6484d211a1bfcd0a72b56d96fba4e30
daniele <d.malitesta@gmail.com>
2021-05-21 15:25:47 +0200

    Deep Style added.

--- elliot/recommender/visual_recommenders/DeepStyle/DeepStyle.py
+++ elliot/recommender/visual_recommenders/DeepStyle/DeepStyle.py
@@ -7,18 +7,16 @@ __version__ = '0.1'
 __author__ = 'Vito Walter Anelli, Claudio Pomo, Daniele Malitesta'
 __email__ = 'vitowalter.anelli@poliba.it, claudio.pomo@poliba.it, daniele.malitesta@poliba.it'
 
-import os
+from tqdm import tqdm
 
 import numpy as np
 import tensorflow as tf
-from tqdm import tqdm
 
-from elliot.dataset.samplers import custom_sampler as cs
 from elliot.recommender import BaseRecommenderModel
 from elliot.recommender.base_recommender_model import init_charger
 from elliot.recommender.recommender_utils_mixin import RecMixin
 from elliot.recommender.visual_recommenders.DeepStyle.DeepStyle_model import DeepStyle_model
-from elliot.utils.write import store_recommendation
+from elliot.recommender.visual_recommenders.DeepStyle import pairwise_pipeline_sampler_deepstyle as ppsd
 
 
 class DeepStyle(RecMixin, BaseRecommenderModel):
@@ -32,6 +30,7 @@ class DeepStyle(RecMixin, BaseRecommenderModel):
         epochs: Number of epochs
         factors: Number of latent factors
         batch_size: Batch size
+        batch_eval: Batch size for evaluation
         l_w: Regularization coefficient
 
     To include the recommendation model, add it to the config file adopting the following pattern:
@@ -46,16 +45,17 @@ class DeepStyle(RecMixin, BaseRecommenderModel):
           epochs: 50
           factors: 100
           batch_size: 128
+          batch_eval: 512
           l_w: 0.000025
     """
     @init_charger
     def __init__(self, data, config, params, *args, **kwargs):
-
         self._params_list = [
+            ("_batch_eval", "batch_eval", "be", 512, int, None),
             ("_factors", "factors", "factors", 100, None, None),
             ("_learning_rate", "lr", "lr", 0.0005, None, None),
             ("_l_w", "l_w", "l_w", 0.000025, None, None),
-            ("_loader", "loader", "load", "ItemAttributes", None, None),
+            ("_loader", "loader", "load", "VisualAttributes", None, None),
         ]
         self.autoset_params()
 
@@ -66,18 +66,25 @@ class DeepStyle(RecMixin, BaseRecommenderModel):
 
         self._side = getattr(self._data.side_information, self._loader, None)
 
-        self._sampler = cs.Sampler(self._data.i_train_dict)
-
         item_indices = [self._side.item_mapping[self._data.private_items[item]] for item in range(self._num_items)]
+
+        self._sampler = ppsd.Sampler(self._data.i_train_dict,
+                                     item_indices,
+                                     self._side.visual_feature_folder_path,
+                                     self._epochs)
+
+        self._next_batch = self._sampler.pipeline(self._data.transactions, self._batch_size)
 
         self._model = DeepStyle_model(self._factors,
                                       self._learning_rate,
                                       self._l_w,
-                                      self._side.visual_features[item_indices],
-                                      self._side.visual_features.shape[1],
+                                      self._side.visual_features_shape,
                                       self._num_users,
                                       self._num_items,
                                       self._seed)
+
+        # only for evaluation purposes
+        self._next_eval_batch = self._sampler.pipeline_eval(self._batch_eval)
 
     @property
     def name(self):
@@ -89,27 +96,34 @@ class DeepStyle(RecMixin, BaseRecommenderModel):
         if self._restore:
             return self.restore_weights()
 
-        for it in self.iterate(self._epochs):
-            loss = 0
-            steps = 0
-            with tqdm(total=int(self._data.transactions // self._batch_size), disable=not self._verbose) as t:
-                for batch in self._sampler.step(self._data.transactions, self._batch_size):
-                    steps += 1
-                    loss += self._model.train_step(batch)
-                    t.set_postfix({'loss': f'{loss.numpy() / steps:.5f}'})
-                    t.update()
+        loss = 0
+        steps = 0
+        it = 0
+        with tqdm(total=int(self._data.transactions // self._batch_size), disable=not self._verbose) as t:
+            for batch in self._next_batch:
+                steps += 1
+                loss += self._model.train_step(batch)
+                t.set_postfix({'loss': f'{loss.numpy() / steps:.5f}'})
+                t.update()
 
-            self.evaluate(it, loss.numpy()/(it + 1))
+                if steps == self._data.transactions // self._batch_size:
+                    t.reset()
+                    self.evaluate(it, loss.numpy() / (it + 1))
+                    it += 1
 
     def get_recommendations(self, k: int = 100):
         predictions_top_k_test = {}
         predictions_top_k_val = {}
-        for index, offset in enumerate(range(0, self._num_users, self._batch_size)):
-            offset_stop = min(offset + self._batch_size, self._num_users)
-            predictions = self._model.predict(offset, offset_stop)
+        for index, offset in enumerate(range(0, self._num_users, self._batch_eval)):
+            offset_stop = min(offset + self._batch_eval, self._num_users)
+            predictions = np.empty((offset_stop - offset, self._num_items))
+            for batch in self._next_eval_batch:
+                item_rel, item_abs, feat = batch
+                p = self._model.predict_item_batch(offset, offset_stop,
+                                                   item_rel[0], item_rel[-1],
+                                                   tf.Variable(feat))
+                predictions[:(offset_stop - offset), item_rel] = p
             recs_val, recs_test = self.process_protocol(k, predictions, offset, offset_stop)
             predictions_top_k_val.update(recs_val)
             predictions_top_k_test.update(recs_test)
         return predictions_top_k_val, predictions_top_k_test
-
-
---
+++ elliot/recommender/visual_recommenders/DeepStyle/pairwise_pipeline_sampler_deepstyle.py
@@ -0,0 +1,118 @@
+"""
+Module description:
+
+"""
+
+__version__ = '0.1'
+__author__ = 'Vito Walter Anelli, Claudio Pomo, Daniele Malitesta, Felice Antonio Merra'
+__email__ = 'vitowalter.anelli@poliba.it, claudio.pomo@poliba.it, daniele.malitesta@poliba.it, felice.merra@poliba.it'
+
+import tensorflow as tf
+import os
+
+import numpy as np
+import random
+np.random.seed(42)
+random.seed(42)
+
+
+class Sampler:
+    def __init__(self, indexed_ratings, item_indices, cnn_features_path, epochs):
+        self._indexed_ratings = indexed_ratings
+        self._item_indices = item_indices
+        self._users = list(self._indexed_ratings.keys())
+        self._nusers = len(self._users)
+        self._items = list({k for a in self._indexed_ratings.values() for k in a.keys()})
+        self._nitems = len(self._items)
+        self._ui_dict = {u: list(set(indexed_ratings[u])) for u in indexed_ratings}
+        self._lui_dict = {u: len(v) for u, v in self._ui_dict.items()}
+
+        self._cnn_features_path = cnn_features_path
+        self._epochs = epochs
+
+    def read_features_triple(self, user, pos, neg):
+        # load positive and negative item features
+        feat_pos = np.load(os.path.join(self._cnn_features_path, str(pos.numpy())) + '.npy')
+        feat_neg = np.load(os.path.join(self._cnn_features_path, str(neg.numpy())) + '.npy')
+
+        return user.numpy(), pos.numpy(), feat_pos, neg.numpy(), feat_neg
+
+    def step(self, events: int, batch_size: int):
+        r_int = np.random.randint
+        n_users = self._nusers
+        n_items = self._nitems
+        ui_dict = self._ui_dict
+        lui_dict = self._lui_dict
+
+        actual_inter = (events // batch_size) * batch_size * self._epochs
+
+        counter_inter = 1
+
+        def sample():
+            u = r_int(n_users)
+            ui = ui_dict[u]
+            lui = lui_dict[u]
+            if lui == n_items:
+                sample()
+            i = ui[r_int(lui)]
+
+            j = r_int(n_items)
+            while j in ui:
+                j = r_int(n_items)
+            return u, i, j
+
+        for ep in range(self._epochs):
+            for _ in range(events):
+                yield sample()
+                if counter_inter == actual_inter:
+                    return
+                else:
+                    counter_inter += 1
+
+    def pipeline(self, num_users, batch_size):
+        def load_func(u, p, n):
+            b = tf.py_function(
+                self.read_features_triple,
+                (u, p, n,),
+                (np.int64, np.int64, np.float32, np.int64, np.float32)
+            )
+            return b
+
+        data = tf.data.Dataset.from_generator(generator=self.step,
+                                              output_shapes=((), (), ()),
+                                              output_types=(tf.int64, tf.int64, tf.int64),
+                                              args=(num_users, batch_size))
+        data = data.map(load_func, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        data = data.batch(batch_size=batch_size)
+        data = data.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+        return data
+
+    def step_eval(self):
+        for i_rel, i_abs in enumerate(self._item_indices):
+            yield i_rel, i_abs
+
+    # this is only for evaluation
+    def pipeline_eval(self, batch_size):
+        def load_func(i_r, i_a):
+            b = tf.py_function(
+                self.read_features,
+                (i_r, i_a,),
+                (np.int64, np.int64, np.float32)
+            )
+            return b
+
+        data = tf.data.Dataset.from_generator(generator=self.step_eval,
+                                              output_shapes=((), ()),
+                                              output_types=(tf.int64, tf.int64))
+        data = data.map(load_func, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        data = data.batch(batch_size=batch_size)
+        data = data.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+        return data
+
+    # this is only for evaluation
+    def read_features(self, item_rel, item_abs):
+        feat = np.load(os.path.join(self._cnn_features_path, str(item_abs.numpy())) + '.npy')
+
+        return item_rel, item_abs, feat
