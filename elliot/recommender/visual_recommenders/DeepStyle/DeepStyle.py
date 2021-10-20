9cc629544f28cf4b0b521bd484f70a248c08946c
daniele <d.malitesta@gmail.com>
2021-10-20 16:29:06 +0200

    NVIDIA.

--- elliot/recommender/visual_recommenders/DeepStyle/DeepStyle.py
+++ elliot/recommender/visual_recommenders/DeepStyle/DeepStyle.py
@@ -12,6 +12,8 @@ from tqdm import tqdm
 import numpy as np
 import tensorflow as tf
 
+import time
+
 from elliot.recommender import BaseRecommenderModel
 from elliot.recommender.base_recommender_model import init_charger
 from elliot.recommender.recommender_utils_mixin import RecMixin
@@ -100,6 +102,7 @@ class DeepStyle(RecMixin, BaseRecommenderModel):
         steps = 0
         it = 0
         with tqdm(total=int(self._data.transactions // self._batch_size), disable=not self._verbose) as t:
+            start_epoch = time.time()
             for batch in self._next_batch:
                 steps += 1
                 loss += self._model.train_step(batch)
@@ -107,6 +110,8 @@ class DeepStyle(RecMixin, BaseRecommenderModel):
                 t.update()
 
                 if steps == self._data.transactions // self._batch_size:
+                    end_epoch = time.time()
+                    print(end_epoch - start_epoch)
                     t.reset()
                     self.evaluate(it, loss.numpy() / steps)
                     it += 1
