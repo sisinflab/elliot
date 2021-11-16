a4d6cee42c42adc2079e56d2044b67dbd0da96f7
walter <vitowalteranelli@gmail.com>
2021-11-16 19:32:59 +0100

    kgrec for KTUP

--- elliot/dataset/modular_loaders/loaders.py
+++ elliot/dataset/modular_loaders/loaders.py
@@ -5,3 +5,4 @@ from elliot.dataset.modular_loaders.generic.item_attributes import ItemAttribute
 from elliot.dataset.modular_loaders.kg.minervini_style import KGCompletion
 from elliot.dataset.modular_loaders.visual.visual_attribute import VisualAttribute
 from elliot.dataset.modular_loaders.textual.textual_attribute import TextualAttribute
+from elliot.dataset.modular_loaders.kg.kgrec import KGRec
