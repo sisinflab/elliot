56c30c826307cfbc433be1673076b70312661a4c
daniele <d.malitesta@gmail.com>
2021-11-24 10:28:08 +0100

    Fixed bug on import of external models.

--- external/models/__init__.py
+++ external/models/__init__.py
@@ -8,23 +8,23 @@ def import_model_by_backend(tensorflow_cmd, pytorch_cmd):
             break
 
 
-from .most_popular import MostPop
-from .msap import MSAPMF
-from .AdversarialMF import AdversarialMF
-from .ktup import KTUP
-from .kgflex import KGFlex
+from .most_popular.most_popular import MostPop
+from .msap.MSAPMF import MSAPMF
+from .AdversarialMF.AdversarialMF import AdversarialMF
+from .ktup.KTUP import KTUP
+from .kgflex.KGFlex import KGFlex
 
 import sys
 for _backend in sys.modules["external"].backend:
     if _backend == "tensorflow":
         pass
     elif _backend == "pytorch":
-        from .ngcf import NGCF
-        from .lightgcn import LightGCN
-        from .pinsage import PinSage
-        from .gat import GAT
-        from .gcmc import GCMC
-        from .disen_gcn import DisenGCN
-        from .mmgcn import MMGCN
-        from .dgcf import DGCF
-        from .egcf import EGCF
+        from .ngcf.NGCF import NGCF
+        from .lightgcn.LightGCN import LightGCN
+        from .pinsage.PinSage import PinSage
+        from .gat.GAT import GAT
+        from .gcmc.GCMC import GCMC
+        from .disen_gcn.DisenGCN import DisenGCN
+        from .mmgcn.MMGCN import MMGCN
+        from .dgcf.DGCF import DGCF
+        from .egcf.EGCF import EGCF
