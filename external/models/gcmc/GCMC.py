722ac07679db9f2af802e5d08d1e1141fd91dc48
danielemalitesta <d.malitesta@gmail.com>
2021-12-09 15:33:19 +0100

    Added save and load model to pytorch-based models.

--- external/models/gcmc/GCMC.py
+++ external/models/gcmc/GCMC.py
@@ -11,7 +11,10 @@ from ast import literal_eval as make_tuple
 
 from tqdm import tqdm
 import numpy as np
+import torch
+import os
 
+from elliot.utils.write import store_recommendation
 from elliot.dataset.samplers import pointwise_pos_neg_ratio_ratings_sampler as ppnrrs
 from elliot.recommender import BaseRecommenderModel
 from elliot.recommender.base_recommender_model import init_charger
@@ -140,3 +143,54 @@ class GCMC(RecMixin, BaseRecommenderModel):
         items_ratings_pair = [list(zip(map(self._data.private_items.get, u_list[0]), u_list[1]))
                               for u_list in list(zip(i.detach().cpu().numpy(), v.detach().cpu().numpy()))]
         return dict(zip(map(self._data.private_users.get, range(offset, offset_stop)), items_ratings_pair))
+
+    def evaluate(self, it=None, loss=0):
+        if (it is None) or (not (it + 1) % self._validation_rate):
+            recs = self.get_recommendations(self.evaluator.get_needed_recommendations())
+            result_dict = self.evaluator.eval(recs)
+
+            self._losses.append(loss)
+
+            self._results.append(result_dict)
+
+            if it is not None:
+                self.logger.info(f'Epoch {(it + 1)}/{self._epochs} loss {loss/(it + 1):.5f}')
+            else:
+                self.logger.info(f'Finished')
+
+            if self._save_recs:
+                self.logger.info(f"Writing recommendations at: {self._config.path_output_rec_result}")
+                if it is not None:
+                    store_recommendation(recs[1], os.path.abspath(
+                        os.sep.join([self._config.path_output_rec_result, f"{self.name}_it={it + 1}.tsv"])))
+                else:
+                    store_recommendation(recs[1], os.path.abspath(
+                        os.sep.join([self._config.path_output_rec_result, f"{self.name}.tsv"])))
+
+            if (len(self._results) - 1) == self.get_best_arg():
+                if it is not None:
+                    self._params.best_iteration = it + 1
+                self.logger.info("******************************************")
+                self.best_metric_value = self._results[-1][self._validation_k]["val_results"][self._validation_metric]
+                if self._save_weights:
+                    if hasattr(self, "_model"):
+                        torch.save({
+                            'model_state_dict': self._model.state_dict(),
+                            'optimizer_state_dict': self._model.optimizer.state_dict()
+                        }, self._saving_filepath)
+                    else:
+                        self.logger.warning("Saving weights FAILED. No model to save.")
+
+    def restore_weights(self):
+        try:
+            checkpoint = torch.load(self._saving_filepath)
+            self._model.load_state_dict(checkpoint['model_state_dict'])
+            self._model.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            print(f"Model correctly Restored")
+            self.evaluate()
+            return True
+
+        except Exception as ex:
+            raise Exception(f"Error in model restoring operation! {ex}")
+
+        return False
