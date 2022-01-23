914cd18757b5eab6695c8d40524b6766149cff6a
daniele <d.malitesta@gmail.com>
2022-01-23 17:30:07 +0100

    Fixed bugs on DiseGCN

--- external/models/disen_gcn/FeatureProjection.py
+++ external/models/disen_gcn/FeatureProjection.py
@@ -7,19 +7,15 @@ class FeatureProjection(torch.nn.Module, ABC):
     def __init__(self, in_channels, out_channels, disen_k):
         super(FeatureProjection, self).__init__()
         self.W = torch.nn.Parameter(
-            torch.nn.init.xavier_normal_(torch.empty((disen_k, in_channels, out_channels // disen_k))))
+            torch.nn.init.xavier_normal_(torch.empty((disen_k, out_channels // disen_k, in_channels))))
         self.b = torch.nn.Parameter(
             torch.nn.init.xavier_normal_(torch.empty((disen_k, out_channels // disen_k))))
         self.relu = torch.nn.ReLU()
         self.disen_k = disen_k
 
     def forward(self, x):
-        # z = self.relu(torch.matmul(self.W.permute(0, 2, 1), x.permute(1, 0)).permute(2, 0, 1) +
-        #               torch.unsqueeze(self.b, 0))
-        # z = z / torch.unsqueeze(torch.unsqueeze(torch.norm(z, 2, dim=[0, 2]), 0), 2)
-
-        z = self.relu(torch.matmul(torch.unsqueeze(x, 0), self.W) +
+        z = self.relu(torch.matmul(self.W, x.permute(1, 0)).permute(2, 0, 1) +
                       torch.unsqueeze(self.b, 0))
-        z = z / torch.unsqueeze(torch.unsqueeze(torch.norm(z, 2, dim=[0, 2]), 0), 2)
+        # z = z / torch.unsqueeze(torch.unsqueeze(torch.norm(z, 2, dim=[0, 2]), 0), 2)
 
         return z
