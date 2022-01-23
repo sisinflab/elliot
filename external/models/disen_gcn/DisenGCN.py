ae4c64730212233541aaf81d3c6fb222470dffcf
daniele <d.malitesta@gmail.com>
2022-01-23 12:16:41 +0100

    Fixed bugs on DiseGCN

--- external/models/disen_gcn/DisenGCN.py
+++ external/models/disen_gcn/DisenGCN.py
@@ -67,17 +67,17 @@ class DisenGCN(RecMixin, BaseRecommenderModel):
         ######################################
 
         self._params_list = [
-            ("_learning_rate", "lr", "lr", 0.0005, None, None),
-            ("_factors", "factors", "factors", 64, None, None),
-            ("_l_w", "l_w", "l_w", 0.01, None, None),
+            ("_learning_rate", "lr", "lr", 0.0005, float, None),
+            ("_factors", "factors", "factors", 64, int, None),
+            ("_l_w", "l_w", "l_w", 0.01, float, None),
             ("_weight_size", "weight_size", "weight_size", "(64,)", lambda x: list(make_tuple(x)),
              lambda x: self._batch_remove(str(x), " []").replace(",", "-")),
             ("_message_dropout", "message_dropout", "message_dropout", "()", lambda x: list(make_tuple(x)),
              lambda x: self._batch_remove(str(x), " []").replace(",", "-")),
             ("_disen_k", "disen_k", "disen_k", "()", lambda x: list(make_tuple(x)),
              lambda x: self._batch_remove(str(x), " []").replace(",", "-")),
-            ("_temperature", "temperature", "temperature", 10, None, None),
-            ("_routing_iterations", "routing_iterations", "routing_iterations", 5, None, None)
+            ("_temperature", "temperature", "temperature", 10, float, None),
+            ("_routing_iterations", "routing_iterations", "routing_iterations", 5, int, None)
         ]
         self.autoset_params()
 
