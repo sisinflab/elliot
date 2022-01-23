be9ec81493081b79f9a57439bab5fe32c007d108
daniele <d.malitesta@gmail.com>
2022-01-23 17:18:23 +0100

    Fixed bugs on DiseGCN

--- external/models/disen_gcn/FeatureProjection.py
+++ external/models/disen_gcn/FeatureProjection.py
@@ -14,7 +14,11 @@ class FeatureProjection(torch.nn.Module, ABC):
         self.disen_k = disen_k
 
     def forward(self, x):
-        z = self.relu(torch.matmul(self.W.permute(0, 2, 1), x.permute(1, 0)).permute(2, 0, 1) +
+        # z = self.relu(torch.matmul(self.W.permute(0, 2, 1), x.permute(1, 0)).permute(2, 0, 1) +
+        #               torch.unsqueeze(self.b, 0))
+        # z = z / torch.unsqueeze(torch.unsqueeze(torch.norm(z, 2, dim=[0, 2]), 0), 2)
+
+        z = self.relu(torch.matmul(torch.unsqueeze(x, 0), self.W) +
                       torch.unsqueeze(self.b, 0))
         z = z / torch.unsqueeze(torch.unsqueeze(torch.norm(z, 2, dim=[0, 2]), 0), 2)
 
