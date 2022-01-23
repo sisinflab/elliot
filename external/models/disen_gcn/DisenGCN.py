742af4b82548ade9c3aaea42a5cc7022b65be233
daniele <d.malitesta@gmail.com>
2022-01-23 10:32:37 +0100

    DGCF completed and tested.

--- external/models/disen_gcn/DisenGCN.py
+++ external/models/disen_gcn/DisenGCN.py
@@ -117,7 +117,7 @@ class DisenGCN(RecMixin, BaseRecommenderModel):
             loss = 0
             steps = 0
             with tqdm(total=int(self._data.transactions // self._batch_size), disable=not self._verbose) as t:
-                for batch in self._sampler.step(self._data.transactions):
+                for batch in self._sampler.step(self._data.transactions, self._batch_size):
                     steps += 1
                     loss += self._model.train_step(batch)
                     t.set_postfix({'loss': f'{loss / steps:.5f}'})
