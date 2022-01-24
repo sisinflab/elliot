c5eec63d6b53e8943ee50397729f8c4b71bcf3d7
daniele <d.malitesta@gmail.com>
2022-01-24 14:52:10 +0100

    Fixed bugs on EGCF

--- external/models/egcf/EGCFModel.py
+++ external/models/egcf/EGCFModel.py
@@ -4,8 +4,8 @@ Module description:
 """
 
 __version__ = '0.3.0'
-__author__ = 'Vito Walter Anelli, Claudio Pomo, Daniele Malitesta, Felice Antonio Merra'
-__email__ = 'vitowalter.anelli@poliba.it, claudio.pomo@poliba.it, daniele.malitesta@poliba.it, felice.merra@poliba.it'
+__author__ = 'Vito Walter Anelli, Claudio Pomo, Daniele Malitesta'
+__email__ = 'vitowalter.anelli@poliba.it, claudio.pomo@poliba.it, daniele.malitesta@poliba.it'
 
 from abc import ABC
 
