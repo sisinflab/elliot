b0b2a1ce8f566e724c3355c5788e209ab9dc669f
daniele <d.malitesta@gmail.com>
2022-01-24 11:11:07 +0100

    Fixed bugs on DiseGCN

--- external/models/disen_gcn/DisenGCNModel.py
+++ external/models/disen_gcn/DisenGCNModel.py
@@ -125,9 +125,7 @@ class DisenGCNModel(torch.nn.Module, ABC):
         difference = torch.clamp(xu_pos - xu_neg, -80.0, 1e8)
         loss = torch.sum(self.softplus(-difference))
         reg_loss = self.l_w * (torch.norm(self.Gu, 2) +
-                               torch.norm(self.Gi, 2) +
-                               torch.stack([torch.norm(value, 2) for value in self.disengcn_network.parameters()],
-                                           dim=0).sum(dim=0)) * 2
+                               torch.norm(self.Gi, 2)) * 2
         loss += reg_loss
 
         self.optimizer.zero_grad()
