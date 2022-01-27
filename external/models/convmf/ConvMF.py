1b62bb89e1cf69e7985d912666f3902dd5e5a8ed
Claudio Pomo <claudio.pomo@poliba.it>
2022-01-27 12:00:09 +0100

    add convmf

--- external/models/convmf/ConvMF.py
+++ external/models/convmf/ConvMF.py
@@ -7,6 +7,7 @@ __version__ = '0.1'
 __author__ = 'Vito Walter Anelli, Claudio Pomo'
 __email__ = 'vitowalter.anelli@poliba.it, claudio.pomo@poliba.it'
 
+import numpy as np
 from tqdm import tqdm
 
 from elliot.recommender import BaseRecommenderModel
