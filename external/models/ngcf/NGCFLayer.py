eee7b5c71145c6b57a1a040233825c013dac4f53
danielemalitesta <d.malitesta@gmail.com>
2022-02-03 17:01:48 +0100

    NGCF bugs fixed.

--- external/models/ngcf/NGCFLayer.py
+++ external/models/ngcf/NGCFLayer.py
@@ -6,12 +6,11 @@ from torch_geometric.utils import degree
 
 
 class NGCFLayer(MessagePassing, ABC):
-    def __init__(self, in_channels, out_channels, message_dropout):
+    def __init__(self, in_channels, out_channels):
         super(NGCFLayer, self).__init__(aggr='add')
         self.lin1 = torch.nn.Linear(in_channels, out_channels)
         self.lin2 = torch.nn.Linear(in_channels, out_channels)
         self.leaky_relu = torch.nn.LeakyReLU()
-        self.dropout = torch.nn.Dropout(message_dropout)
 
     def forward(self, x, edge_index):
         row, col = edge_index
@@ -22,7 +21,7 @@ class NGCFLayer(MessagePassing, ABC):
         deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
         norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
 
-        return self.dropout(self.leaky_relu(self.lin1(x) + self.propagate(edge_index, x=x, norm=norm)))
+        return self.leaky_relu(self.lin1(x) + self.propagate(edge_index, x=x, norm=norm))
 
     def message(self, x_i, x_j, norm):
         return norm.view(-1, 1) * (self.lin1(x_j) + self.lin2(x_i * x_j))
