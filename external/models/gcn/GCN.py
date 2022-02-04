5ddf4d3d2763159e3455e6799039c85a1bf0448b
daniele <d.malitesta@gmail.com>
2022-02-04 18:05:03 +0100

    GCN random seed try.

--- external/models/gcn/GCN.py
+++ external/models/gcn/GCN.py
@@ -76,7 +76,8 @@ class GCN(RecMixin, BaseRecommenderModel):
         col = [c + self._num_users for c in col]
         edge_index = np.array([row, col])
         edge_index = torch.tensor(edge_index, dtype=torch.int64)
-        self.adj = SparseTensor(row=edge_index[0], col=edge_index[1], sparse_sizes=(self._num_users, self._num_items))
+        self.adj = SparseTensor(row=edge_index[0], col=edge_index[1], sparse_sizes=(self._num_users + self._num_items,
+                                                                                    self._num_users + self._num_items))
 
         self._model = GCNModel(
             num_users=self._num_users,
