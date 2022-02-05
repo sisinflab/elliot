bb3d77d749336166ecf30619696a8da271121e15
daniele <d.malitesta@gmail.com>
2022-02-05 10:17:31 +0100

    Bug on regularization fixed.

--- external/models/lightgcn/LightGCNModel.py
+++ external/models/lightgcn/LightGCNModel.py
@@ -105,7 +105,7 @@ class LightGCNModel(torch.nn.Module, ABC):
         difference = torch.clamp(xu_pos - xu_neg, -80.0, 1e8)
         loss = torch.sum(self.softplus(-difference))
         reg_loss = self.l_w * (torch.norm(self.Gu, 2) +
-                               torch.norm(self.Gi, 2)) * 2
+                               torch.norm(self.Gi, 2))
         loss += reg_loss
 
         self.optimizer.zero_grad()
