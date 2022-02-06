6afed9716745bf8e0da581fb25f90d9af4d773d4
daniele <d.malitesta@gmail.com>
2022-02-06 17:44:42 +0100

    GCN random seed try.

--- external/models/egcfv2/NodeNodeTextLayer.py
+++ external/models/egcfv2/NodeNodeTextLayer.py
@@ -14,14 +14,15 @@ class NodeNodeTextLayer(MessagePassing, ABC):
         self.leaky_relu = torch.nn.LeakyReLU()
 
     def forward(self, x, edge_index, node_attr, edge_attr):
-        if self.normalize:
-            edge_index = gcn_norm(edge_index, None, x.size(self.node_dim), add_self_loops=True, dtype=x.dtype)
-
-        print(edge_index)
-
         edge_attr = self.leaky_relu(self.lin1(edge_attr))
         weights = torch.nn.functional.cosine_similarity(torch.mul(node_attr, edge_attr), node_attr, dim=1)
         edge_index = mul_nnz(edge_index, weights)
+
+        print(edge_index)
+        exit()
+
+        if self.normalize:
+            edge_index = gcn_norm(edge_index, None, x.size(self.node_dim), add_self_loops=True, dtype=x.dtype)
 
         return self.propagate(edge_index, x=x)
 
