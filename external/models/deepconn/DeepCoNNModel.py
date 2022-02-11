60fd08d244db70a1a559b7bb3864307d618440fb
daniele <d.malitesta@gmail.com>
2022-02-11 14:06:58 +0100

    GCN random seed try.

--- external/models/deepconn/DeepCoNNModel.py
+++ external/models/deepconn/DeepCoNNModel.py
@@ -199,10 +199,8 @@ class DeepCoNNModel(tf.keras.Model, ABC):
         out_2 = tf.matmul(tf.square(out), tf.square(self.W2))
 
         out_inter = self.dropout(tf.constant(0.5) * (tf.square(out_1) - out_2), training=False)
-        print(out_inter.shape)
-        exit()
         out_inter = tf.reduce_sum(out_inter, -1, keepdims=True)
-        rui = tf.squeeze(self.sigmoid(self.B + out_inter + one))
+        rui = self.sigmoid(self.B + out_inter + one)
         return tf.reshape(rui, [out_users.shape[0], out_items.shape[0]])
 
     @tf.function
