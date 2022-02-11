06795d6f57dd4aeaf32246e4f61730ab63fdd7f0
daniele <d.malitesta@gmail.com>
2022-02-11 12:54:25 +0100

    GCN random seed try.

--- external/models/deepconn/DeepCoNNModel.py
+++ external/models/deepconn/DeepCoNNModel.py
@@ -51,43 +51,45 @@ class DeepCoNNModel(tf.keras.Model, ABC):
 
         self.textual_words_feature_shape = textual_words_feature_shape
 
-        self.initializer = tf.initializers.GlorotUniform()
-
         # cnn for user and item reviews
         self.user_review_cnn_network = []
         self.user_review_cnn_network.append(
             (tf.Variable(
-                initial_value=self.initializer([self.user_review_cnn_kernel[0],
-                                                self.textual_words_feature_shape,
-                                                1,
-                                                self.user_review_cnn_features[0]])),
-             tf.Variable(initial_value=tf.zeros([1, self.user_review_cnn_features[0]])))
+                initial_value=tf.random.truncated_normal([self.user_review_cnn_kernel[0],
+                                                          self.textual_words_feature_shape,
+                                                          1,
+                                                          self.user_review_cnn_features[0]], stddev=0.1)),
+             tf.Variable(initial_value=tf.constant(0.1, [1, self.user_review_cnn_features[0]])))
         )
         for layer in range(1, len(self.user_review_cnn_features)):
             self.user_review_cnn_network.append((tf.Variable(
-                initial_value=self.initializer([self.user_review_cnn_kernel[layer],
-                                                self.textual_words_feature_shape,
-                                                self.user_review_cnn_features[layer - 1],
-                                                self.user_review_cnn_features[layer]])),
-                                                 tf.Variable(initial_value=tf.zeros(
-                                                     [1, self.user_review_cnn_features[layer]]))))
+                initial_value=tf.random.truncated_normal([self.user_review_cnn_kernel[layer],
+                                                          self.textual_words_feature_shape,
+                                                          self.user_review_cnn_features[layer - 1],
+                                                          self.user_review_cnn_features[layer]], stddev=0.1)),
+                                                 tf.Variable(initial_value=tf.constant(0.1,
+                                                                                       [1,
+                                                                                        self.user_review_cnn_features[
+                                                                                            layer]]))))
         self.item_review_cnn_network = []
         self.item_review_cnn_network.append(
             (tf.Variable(
-                initial_value=self.initializer([self.item_review_cnn_kernel[0],
-                                                self.textual_words_feature_shape,
-                                                1,
-                                                self.item_review_cnn_features[0]])),
-             tf.Variable(initial_value=tf.zeros([1, self.item_review_cnn_features[0]])))
+                initial_value=tf.random.truncated_normal([self.item_review_cnn_kernel[0],
+                                                          self.textual_words_feature_shape,
+                                                          1,
+                                                          self.item_review_cnn_features[0]], stddev=0.1)),
+             tf.Variable(initial_value=tf.constant(0.1, [1, self.item_review_cnn_features[0]])))
         )
         for layer in range(1, len(self.item_review_cnn_features)):
             self.item_review_cnn_network.append((tf.Variable(
-                initial_value=self.initializer([self.item_review_cnn_kernel[0],
-                                                self.textual_words_feature_shape,
-                                                self.item_review_cnn_features[layer - 1],
-                                                self.item_review_cnn_features[layer]])),
-                                                 tf.Variable(initial_value=tf.zeros(
-                                                     [1, self.item_review_cnn_features[layer]]))))
+                initial_value=tf.random.truncated_normal([self.item_review_cnn_kernel[0],
+                                                          self.textual_words_feature_shape,
+                                                          self.item_review_cnn_features[layer - 1],
+                                                          self.item_review_cnn_features[layer]], stddev=0.1)),
+                                                 tf.Variable(initial_value=tf.constant(0.1,
+                                                                                       [1,
+                                                                                        self.item_review_cnn_features[
+                                                                                            layer]]))))
 
         # fully-connected layer for users and items
         self.user_fully_connected = tf.keras.layers.Dense(units=self.latent_size, use_bias=True)
@@ -109,9 +111,13 @@ class DeepCoNNModel(tf.keras.Model, ABC):
 
         out_users = []
         for layer in range(len(self.user_review_cnn_network)):
-            out = tf.reduce_max(
+            out = tf.nn.max_pool(
                 tf.nn.conv2d(input=user_reviews_features, filters=self.user_review_cnn_network[layer][0],
-                             strides=[1, 1, 1, 1], padding='VALID') + self.user_review_cnn_network[layer][1], axis=1)
+                             strides=[1, 1, 1, 1], padding='VALID') + self.user_review_cnn_network[layer][1],
+                ksize=[1, user_reviews_features.shape[1] - self.user_review_cnn_kernel[layer] + 1, 1, 1],
+                strides=[1, 1, 1, 1],
+                padding='VALID',
+            )
             out_users.append(out)
         out_users = tf.reshape(tf.concat(out_users, axis=1), [user_reviews.shape[0], -1])
         out_users = self.user_fully_connected(out_users)
@@ -124,9 +130,13 @@ class DeepCoNNModel(tf.keras.Model, ABC):
 
         out_items = []
         for layer in range(len(self.item_review_cnn_network)):
-            out = tf.reduce_max(
+            out = tf.nn.max_pool(
                 tf.nn.conv2d(input=item_reviews_features, filters=self.item_review_cnn_network[layer][0],
-                             strides=[1, 1, 1, 1], padding='VALID') + self.item_review_cnn_network[layer][1], axis=1)
+                             strides=[1, 1, 1, 1], padding='VALID') + self.item_review_cnn_network[layer][1],
+                ksize=[1, item_reviews_features.shape[1] - self.item_review_cnn_kernel[layer] + 1, 1, 1],
+                strides=[1, 1, 1, 1],
+                padding='VALID',
+            )
             out_items.append(out)
         out_items = tf.reshape(tf.concat(out_items, axis=1), [item_reviews.shape[0], -1])
         out_items = self.user_fully_connected(out_items)
@@ -141,18 +151,26 @@ class DeepCoNNModel(tf.keras.Model, ABC):
 
         out_users = []
         for layer in range(len(self.user_review_cnn_network)):
-            out = tf.reduce_max(
+            out = tf.nn.max_pool(
                 tf.nn.conv2d(input=user_reviews_features, filters=self.user_review_cnn_network[layer][0],
-                             strides=[1, 1, 1, 1], padding='VALID') + self.user_review_cnn_network[layer][1], axis=1)
+                             strides=[1, 1, 1, 1], padding='VALID') + self.user_review_cnn_network[layer][1],
+                ksize=[1, user_reviews_features.shape[1] - self.user_review_cnn_kernel[layer] + 1, 1, 1],
+                strides=[1, 1, 1, 1],
+                padding='VALID',
+            )
             out_users.append(out)
         out_users = tf.reshape(tf.concat(out_users, axis=1), [user.shape[0], -1])
         out_users = self.dropout(self.user_fully_connected(out_users), training=training)
 
         out_items = []
         for layer in range(len(self.item_review_cnn_network)):
-            out = tf.reduce_max(
+            out = tf.nn.max_pool(
                 tf.nn.conv2d(input=item_reviews_features, filters=self.item_review_cnn_network[layer][0],
-                             strides=[1, 1, 1, 1], padding='VALID') + self.item_review_cnn_network[layer][1], axis=1)
+                             strides=[1, 1, 1, 1], padding='VALID') + self.item_review_cnn_network[layer][1],
+                ksize=[1, item_reviews_features.shape[1] - self.item_review_cnn_kernel[layer] + 1, 1, 1],
+                strides=[1, 1, 1, 1],
+                padding='VALID',
+            )
             out_items.append(out)
         out_items = tf.reshape(tf.concat(out_items, axis=1), [user.shape[0], -1])
         out_items = self.dropout(self.item_fully_connected(out_items), training=training)
