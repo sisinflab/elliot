330311891912579a2e1c09071eeb44165a4efe66
daniele <d.malitesta@gmail.com>
2022-02-11 21:08:41 +0100

    GCN random seed try.

--- external/models/pinsage/PinSage.py
+++ external/models/pinsage/PinSage.py
@@ -91,6 +91,7 @@ class PinSage(RecMixin, BaseRecommenderModel):
         row, col = data.sp_i_train.nonzero()
         col = [c + self._num_users for c in col]
         edge_index = np.array([row, col])
+        edge_index = torch.tensor(edge_index, dtype=torch.int64)
         self.adj = SparseTensor(row=torch.cat([edge_index[0], edge_index[1]], dim=0),
                                 col=torch.cat([edge_index[1], edge_index[0]], dim=0),
                                 sparse_sizes=(self._num_users + self._num_items,
