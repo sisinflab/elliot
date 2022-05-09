a3e4b7222b6088bb9c9deefae1e6a890cf23f838
danielemalitesta <d.malitesta@gmail.com>
2022-05-09 17:54:02 +0200

    UltraGCN added

---
+++ external/models/ultragcn/UltraGCN.py
@@ -0,0 +1,188 @@
+from tqdm import tqdm
+import numpy as np
+import torch
+import os
+
+from elliot.utils.write import store_recommendation
+from elliot.dataset.samplers import custom_sampler as cs
+from elliot.recommender import BaseRecommenderModel
+from elliot.recommender.base_recommender_model import init_charger
+from elliot.recommender.recommender_utils_mixin import RecMixin
+from .UltraGCNModel import UltraGCNModel
+
+
+class UltraGCN(RecMixin, BaseRecommenderModel):
+    r"""
+    UltraGCN: Ultra Simplification of Graph Convolutional Networks for Recommendation
+
+    For further details, please refer to the `paper <https://dl.acm.org/doi/10.1145/3459637.3482291>`_
+
+    To include the recommendation model, add it to the config file adopting the following pattern:
+
+    .. code:: yaml
+    """
+
+    @init_charger
+    def __init__(self, data, config, params, *args, **kwargs):
+
+        self._sampler = cs.Sampler(self._data.i_train_dict)
+        if self._batch_size < 1:
+            self._batch_size = self._num_users
+
+        ######################################
+
+        self._params_list = [
+            ("_learning_rate", "lr", "lr", 0.0005, float, None),
+            ("_factors", "factors", "factors", 64, int, None),
+            ("_w1", "w1", "w1", 0.01, float, None),
+            ("_w2", "w2", "w2", 0.01, float, None),
+            ("_w3", "w3", "w3", 0.01, float, None),
+            ("_w4", "w4", "w4", 0.01, float, None),
+            ("_ii_neighbor_num", "ii_neighbor_num", "ii_neighbor_num", 10, int, None),
+            ("_initial_weight", "initial_weight", "initial_weight", 0.01, float, None),
+            ("_negative_num", "negative_num", "negative_num", 1500, int, None),
+            ("_negative_weight", "negative_weight", "negative_weight", 300, float, None),
+            ("_gamma", "gamma", "gamma", 0.01, float, None),
+            ("_lambda", "lambda", "lambda", 0.01, float, None)
+        ]
+        self.autoset_params()
+
+        ii_neighbor_mat, ii_constraint_mat, constraint_mat = self.get_ii_constraint_mat(data.sp_i_train,
+                                                                                        self._ii_neighbor_num)
+
+        self._model = UltraGCNModel(
+            num_users=self._num_users,
+            num_items=self._num_items,
+            learning_rate=self._learning_rate,
+            embed_k=self._factors,
+            w1=self._w1,
+            w2=self._w2,
+            w3=self._w3,
+            w4=self._w4,
+            initial_weight=self._initial_weight,
+            negative_num=self._negative_num,
+            negative_weight=self._negative_weight,
+            ii_neighbor_mat=ii_neighbor_mat,
+            ii_constraint_mat=ii_constraint_mat,
+            constraint_mat=constraint_mat,
+            gamma=self._gamma,
+            lm=self._lambda,
+            random_seed=self._seed
+        )
+
+    @property
+    def name(self):
+        return "UltraGCN" \
+               + f"_{self.get_base_params_shortcut()}" \
+               + f"_{self.get_params_shortcut()}"
+
+    @staticmethod
+    def get_ii_constraint_mat(train_mat, num_neighbors, ii_diagonal_zero=False):
+
+        A = train_mat.T.dot(train_mat)  # I * I
+        n_items = A.shape[0]
+        res_mat = torch.zeros((n_items, num_neighbors))
+        res_sim_mat = torch.zeros((n_items, num_neighbors))
+        if ii_diagonal_zero:
+            A[range(n_items), range(n_items)] = 0
+        items_D = np.sum(A, axis=0).reshape(-1)
+        users_D = np.sum(A, axis=1).reshape(-1)
+
+        beta_uD = (np.sqrt(users_D + 1) / users_D).reshape(-1, 1)
+        beta_iD = (1 / np.sqrt(items_D + 1)).reshape(1, -1)
+        all_ii_constraint_mat = torch.from_numpy(beta_uD.dot(beta_iD))
+        for i in range(n_items):
+            row = all_ii_constraint_mat[i] * torch.from_numpy(A.getrow(i).toarray()[0])
+            row_sims, row_idxs = torch.topk(row, num_neighbors)
+            res_mat[i] = row_idxs
+            res_sim_mat[i] = row_sims
+
+        constraint_mat = {"beta_uD": torch.from_numpy(beta_uD).reshape(-1),
+                          "beta_iD": torch.from_numpy(beta_iD).reshape(-1)}
+
+        return res_mat.long(), res_sim_mat.float(), constraint_mat
+
+    def train(self):
+        if self._restore:
+            return self.restore_weights()
+
+        for it in self.iterate(self._epochs):
+            loss = 0
+            steps = 0
+            with tqdm(total=int(self._data.transactions // self._batch_size), disable=not self._verbose) as t:
+                for batch in self._sampler.step(self._data.transactions, self._batch_size):
+                    steps += 1
+                    loss += self._model.train_step(batch)
+                    t.set_postfix({'loss': f'{loss / steps:.5f}'})
+                    t.update()
+
+            self.evaluate(it, loss / (it + 1))
+
+    def get_recommendations(self, k: int = 100):
+        predictions_top_k_test = {}
+        predictions_top_k_val = {}
+        gu, gi = self._model.propagate_embeddings(evaluate=True)
+        for index, offset in enumerate(range(0, self._num_users, self._batch_size)):
+            offset_stop = min(offset + self._batch_size, self._num_users)
+            predictions = self._model.predict(gu[offset: offset_stop], gi)
+            recs_val, recs_test = self.process_protocol(k, predictions, offset, offset_stop)
+            predictions_top_k_val.update(recs_val)
+            predictions_top_k_test.update(recs_test)
+        return predictions_top_k_val, predictions_top_k_test
+
+    def get_single_recommendation(self, mask, k, predictions, offset, offset_stop):
+        v, i = self._model.get_top_k(predictions, mask[offset: offset_stop], k=k)
+        items_ratings_pair = [list(zip(map(self._data.private_items.get, u_list[0]), u_list[1]))
+                              for u_list in list(zip(i.detach().cpu().numpy(), v.detach().cpu().numpy()))]
+        return dict(zip(map(self._data.private_users.get, range(offset, offset_stop)), items_ratings_pair))
+
+    def evaluate(self, it=None, loss=0):
+        if (it is None) or (not (it + 1) % self._validation_rate):
+            recs = self.get_recommendations(self.evaluator.get_needed_recommendations())
+            result_dict = self.evaluator.eval(recs)
+
+            self._losses.append(loss)
+
+            self._results.append(result_dict)
+
+            if it is not None:
+                self.logger.info(f'Epoch {(it + 1)}/{self._epochs} loss {loss / (it + 1):.5f}')
+            else:
+                self.logger.info(f'Finished')
+
+            if self._save_recs:
+                self.logger.info(f"Writing recommendations at: {self._config.path_output_rec_result}")
+                if it is not None:
+                    store_recommendation(recs[1], os.path.abspath(
+                        os.sep.join([self._config.path_output_rec_result, f"{self.name}_it={it + 1}.tsv"])))
+                else:
+                    store_recommendation(recs[1], os.path.abspath(
+                        os.sep.join([self._config.path_output_rec_result, f"{self.name}.tsv"])))
+
+            if (len(self._results) - 1) == self.get_best_arg():
+                if it is not None:
+                    self._params.best_iteration = it + 1
+                self.logger.info("******************************************")
+                self.best_metric_value = self._results[-1][self._validation_k]["val_results"][self._validation_metric]
+                if self._save_weights:
+                    if hasattr(self, "_model"):
+                        torch.save({
+                            'model_state_dict': self._model.state_dict(),
+                            'optimizer_state_dict': self._model.optimizer.state_dict()
+                        }, self._saving_filepath)
+                    else:
+                        self.logger.warning("Saving weights FAILED. No model to save.")
+
+    def restore_weights(self):
+        try:
+            checkpoint = torch.load(self._saving_filepath)
+            self._model.load_state_dict(checkpoint['model_state_dict'])
+            self._model.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            print(f"Model correctly Restored")
+            self.evaluate()
+            return True
+
+        except Exception as ex:
+            raise Exception(f"Error in model restoring operation! {ex}")
+
+        return False
---
+++ external/models/ultragcn/UltraGCNModel.py
@@ -0,0 +1,153 @@
+from abc import ABC
+
+import torch
+import numpy as np
+import random
+
+
+class UltraGCNModel(torch.nn.Module, ABC):
+    def __init__(self,
+                 num_users,
+                 num_items,
+                 learning_rate,
+                 embed_k,
+                 w1,
+                 w2,
+                 w3,
+                 w4,
+                 initial_weight,
+                 negative_num,
+                 negative_weight,
+                 ii_neighbor_mat,
+                 ii_constraint_mat,
+                 constraint_mat,
+                 gamma,
+                 lm,
+                 random_seed,
+                 name="UltraGCN",
+                 **kwargs
+                 ):
+        super().__init__()
+
+        # set seed
+        random.seed(random_seed)
+        np.random.seed(random_seed)
+        torch.manual_seed(random_seed)
+        torch.cuda.manual_seed(random_seed)
+        torch.cuda.manual_seed_all(random_seed)
+        torch.backends.cudnn.deterministic = True
+
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        self.num_users = num_users
+        self.num_items = num_items
+        self.embed_k = embed_k
+        self.learning_rate = learning_rate
+        self.w1 = w1
+        self.w2 = w2
+        self.w3 = w3
+        self.w4 = w4
+        self.initial_weight = initial_weight
+        self.negative_num = negative_num
+        self.negative_weight = negative_weight
+        self.ii_neighbor_mat = ii_neighbor_mat
+        self.ii_constraint_mat = ii_constraint_mat
+        self.constraint_mat = constraint_mat
+        self.gamma = gamma
+        self.lm = lm
+
+        self.Gu = torch.nn.Embedding(self.num_users, self.embed_k)
+        torch.nn.init.normal_(self.Gu.weight, std=self.initial_weight)
+        self.Gu.to(self.device)
+        self.Gi = torch.nn.Embedding(self.num_items, self.embed_k)
+        torch.nn.init.normal_(self.Gi.weight, std=self.initial_weight)
+        self.Gi.to(self.device)
+
+        self.softplus = torch.nn.Softplus()
+
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+
+    def get_omegas(self, users, pos_items, neg_items):
+        if self.w2 > 0:
+            pos_weight = torch.mul(self.constraint_mat['beta_uD'][users], self.constraint_mat['beta_iD'][pos_items]).to(
+                self.device)
+            pos_weight = self.w1 + self.w2 * pos_weight
+        else:
+            pos_weight = self.w1 * torch.ones(len(pos_items)).to(self.device)
+
+        # users = (users * self.item_num).unsqueeze(0)
+        if self.w4 > 0:
+            neg_weight = torch.mul(torch.repeat_interleave(self.constraint_mat['beta_uD'][users], neg_items.size(1)),
+                                   self.constraint_mat['beta_iD'][neg_items.flatten()]).to(self.device)
+            neg_weight = self.w3 + self.w4 * neg_weight
+        else:
+            neg_weight = self.w3 * torch.ones(neg_items.size(0) * neg_items.size(1)).to(self.device)
+
+        weight = torch.cat((pos_weight, neg_weight))
+        return weight
+
+    def cal_loss_l(self, users, pos_items, neg_items, omega_weight):
+        user_embeds = self.Gu(users)
+        pos_embeds = self.Gi(pos_items)
+        neg_embeds = self.Gi(neg_items)
+
+        pos_scores = (user_embeds * pos_embeds).sum(dim=-1)  # batch_size
+        user_embeds = user_embeds.unsqueeze(1)
+        neg_scores = (user_embeds * neg_embeds).sum(dim=-1)  # batch_size * negative_num
+
+        neg_labels = torch.zeros(neg_scores.size()).to(self.device)
+        neg_loss = torch.nn.functional.binary_cross_entropy_with_logits(neg_scores, neg_labels,
+                                                                        weight=omega_weight[len(pos_scores):].view(
+                                                                            neg_scores.size()),
+                                                                        reduction='none').mean(dim=-1)
+
+        pos_labels = torch.ones(pos_scores.size()).to(self.device)
+        pos_loss = torch.nn.functional.binary_cross_entropy_with_logits(pos_scores, pos_labels,
+                                                                        weight=omega_weight[:len(pos_scores)],
+                                                                        reduction='none')
+
+        loss = pos_loss + neg_loss * self.negative_weight
+
+        return loss.sum()
+
+    def cal_loss_i(self, users, pos_items):
+        neighbor_embeds = self.Gi(
+            self.ii_neighbor_mat[pos_items].to(self.device))  # len(pos_items) * num_neighbors * dim
+        sim_scores = self.ii_constraint_mat[pos_items].to(self.device)  # len(pos_items) * num_neighbors
+        user_embeds = self.Gu(users).unsqueeze(1)
+
+        loss = -sim_scores * (user_embeds * neighbor_embeds).sum(dim=-1).sigmoid().log()
+
+        # loss = loss.sum(-1)
+        return loss.sum()
+
+    def norm_loss(self):
+        loss = 0.0
+        for parameter in self.parameters():
+            loss += torch.sum(parameter ** 2)
+        return loss / 2
+
+    def forward(self, users, pos_items, neg_items):
+        omega_weight = self.get_omegas(users, pos_items, neg_items)
+
+        loss = self.cal_loss_l(users, pos_items, neg_items, omega_weight)
+        loss += self.gamma * self.norm_loss()
+        loss += self.lambda_ * self.cal_loss_i(users, pos_items)
+        return loss
+
+    def predict(self, gu, gi, **kwargs):
+        return torch.matmul(gu.to(self.device), torch.transpose(gi.to(self.device), 0, 1))
+
+    def train_step(self, batch):
+        user, pos, neg = batch
+        loss = self.forward(user, pos, neg)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return loss.detach().cpu().numpy()
+
+    def get_top_k(self, preds, train_mask, k=100):
+        return torch.topk(torch.where(torch.tensor(train_mask).to(self.device), preds.to(self.device),
+                                      torch.tensor(-np.inf).to(self.device)), k=k, sorted=True)
