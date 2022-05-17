a6e952d82d8945f9af361049b73279c826331f7c
Daniele Malitesta <57352389+danielemalitesta@users.noreply.github.com>
2022-05-17 21:54:26 +0200

    Update sampling.py

--- external/models/ultragcn/sampling.py
+++ external/models/ultragcn/sampling.py
@@ -4,6 +4,7 @@ import random
 
 np.random.seed(1234)
 random.seed(1234)
+rng = np.random.default_rng()
 
 
 def sampling(pos_train_data, ps, item_num, neg_ratio, interacted_items, sampling_sift_pos):
@@ -12,13 +13,13 @@ def sampling(pos_train_data, ps, item_num, neg_ratio, interacted_items, sampling
     if sampling_sift_pos:
         neg_items = []
         for u, probs in zip(pos_train_data[0], ps):
-            u_neg_items = np.random.Generator.choice(neg_candidates, size=neg_ratio, p=probs, replace=True).reshape(1, -1)
+            u_neg_items = rng.choice(neg_candidates, size=neg_ratio, p=probs, replace=True).reshape(1, -1)
             
             neg_items.append(u_neg_items)
 
         neg_items = np.concatenate(neg_items, axis=0)
     else:
-        neg_items = np.random.Generator.choice(neg_candidates, (len(pos_train_data[0]), neg_ratio), replace=True)
+        neg_items = rng.choice(neg_candidates, (len(pos_train_data[0]), neg_ratio), replace=True)
 
     neg_items = torch.from_numpy(neg_items)
 
