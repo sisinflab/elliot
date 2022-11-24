e0d17a1c66c45a1544d10067dd1b86f66daf5ebe
danielemalitesta <d.malitesta@gmail.com>
2022-11-24 10:08:04 +0100

    Added Adomavicius stats (need to be completed).

--- start_experiments.py
+++ start_experiments.py
@@ -2,7 +2,7 @@ from elliot.run import run_experiment
 import argparse
 
 parser = argparse.ArgumentParser(description="Run sample main.")
-parser.add_argument('--config', type=str, default='svdgcn')
+parser.add_argument('--config', type=str, default='split')
 args = parser.parse_args()
 
 run_experiment(f"config_files/{args.config}.yml")
