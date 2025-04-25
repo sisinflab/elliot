0bb0b09a3da9bcf47751b955acfa895b6eccb167
Francesco Falcone <f.falcone3@studenti.poliba.it>
2025-04-25 20:32:21 +0200

    DataSet class partial refactoring

--- elliot/dataset/dataset.py
+++ elliot/dataset/dataset.py
@@ -41,10 +41,10 @@ class DataSetRequiredAttributesController(type):
 
     def __call__(cls, *args, **kwargs):
         class_object = type.__call__(cls, *args, **kwargs)
-        cls.check_required_attributes(class_object)
+        cls._check_required_attributes(class_object)
         return class_object
 
-    def check_required_attributes(cls, class_object):
+    def _check_required_attributes(cls, class_object):
         missing_attrs = [f"{attr}" for attr in cls.required_attributes
                          if not hasattr(class_object, attr)]
         if missing_attrs:
@@ -70,18 +70,21 @@ class DataSet(metaclass=DataSetRequiredAttributesController):
         self.args = args
         self.kwargs = kwargs
 
+        self._handle_train_set(side_information_data, data_tuple)
+        self._handle_val_test_sets(data_tuple)
+
+    def _handle_train_set(self, side_information_data, data_tuple):
+        self.train_dict = self._dataframe_to_dict(data_tuple[0])
+
         if self.config.align_side_with_train == True:
-            self.side_information = self.align_with_training(train=data_tuple[0],
-                                                             side_information_data=side_information_data)
+            self.side_information = self._align_with_training(side_information_data=side_information_data)
         else:
             self.side_information = side_information_data
 
-        self.train_dict = self.dataframe_to_dict(data_tuple[0])
-
-        self.users = list(self.train_dict.keys())
-        self.items = list({k for a in self.train_dict.values() for k in a.keys()})
+        self.users, self.items = self._compute_users_items(self.train_dict)
         self.num_users = len(self.users)
         self.num_items = len(self.items)
+
         self.transactions = sum(len(v) for v in self.train_dict.values())
 
         sparsity = 1 - (self.transactions / (self.num_users * self.num_items))
@@ -97,43 +100,55 @@ class DataSet(metaclass=DataSetRequiredAttributesController):
         self.i_train_dict = {self.public_users[user]: {self.public_items[i]: v for i, v in items.items()}
                              for user, items in self.train_dict.items()}
 
-        self.sp_i_train = self.build_sparse()
-        self.sp_i_train_ratings = self.build_sparse_ratings()
-
-        if len(data_tuple) == 2:
-            self.test_dict = self.build_dict(data_tuple[1])
-            if hasattr(config, "negative_sampling"):
-                val_neg_samples, test_neg_samples = NegativeSampler.sample(config, self.public_users, self.public_items,
-                                                                           self.private_users, self.private_items,
-                                                                           self.sp_i_train, None, self.test_dict)
-                sp_i_test = self.to_bool_sparse(self.test_dict)
-                test_candidate_items = test_neg_samples + sp_i_test
-                self.test_mask = np.where((test_candidate_items.toarray() == True), True, False)
-        else:
-            self.val_dict = self.build_dict(data_tuple[1])
-            self.test_dict = self.build_dict(data_tuple[2])
-            if hasattr(config, "negative_sampling"):
-                val_neg_samples, test_neg_samples = NegativeSampler.sample(config, self.public_users, self.public_items,
-                                                                           self.private_users, self.private_items,
-                                                                           self.sp_i_train, self.val_dict,
-                                                                           self.test_dict)
-                sp_i_val = self.to_bool_sparse(self.val_dict)
-                sp_i_test = self.to_bool_sparse(self.test_dict)
-                val_candidate_items = val_neg_samples + sp_i_val
-                self.val_mask = np.where((val_candidate_items.toarray() == True), True, False)
-                test_candidate_items = test_neg_samples + sp_i_test
-                self.test_mask = np.where((test_candidate_items.toarray() == True), True, False)
-
+        self.sp_i_train = self._build_sparse(self.i_train_dict, self.users, self.items)
+        self.sp_i_train_ratings = self._build_sparse_ratings(self.i_train_dict, self.users, self.items)
         self.allunrated_mask = np.where((self.sp_i_train.toarray() == 0), True, False)
 
-    def build_items_neighbour(self):
-        row, col = self.sp_i_train.nonzero()
-        edge_index = np.array([row, col])
-        iu_dict = {i: edge_index[0, iu].tolist() for i, iu in
-                   enumerate(list((edge_index[1] == i).nonzero()[0] for i in list(self.private_items.keys())))}
-        return iu_dict
+    def _handle_val_test_sets(self, data_tuple):
+        if len(data_tuple) == 2:
+            self.val_dict = None
+            self.test_dict = self._dataframe_to_dict(data_tuple[1])
+        else:
+            self.val_dict = self._dataframe_to_dict(data_tuple[1])
+            self.test_dict = self._dataframe_to_dict(data_tuple[2])
 
-    def dataframe_to_dict(self, data):
+        self._handle_negative_sampling()
+
+        self.val_users, self.val_items = self._compute_users_items(self.val_dict)
+        self.test_users, self.test_items = self._compute_users_items(self.test_dict)
+
+        #self.sp_i_val = self._build_sparse(self.i_val_dict, self.users, self.items)
+        #self.sp_i_val_ratings = self._build_sparse_ratings(self.i_val_dict, self.users, self.items)
+
+    def _compute_users_items(self, dict):
+        users = list(dict.keys())
+        item_set = set()
+        for user_ratings in dict.values():
+            item_set.update(user_ratings.keys())
+        items = list(item_set)
+        return users, items
+
+    def _handle_negative_sampling(self):
+        if hasattr(self.config, "negative_sampling"):
+            val_neg_samples, test_neg_samples = NegativeSampler.sample(
+                self.config,
+                self.public_users,
+                self.public_items,
+                self.private_users,
+                self.private_items,
+                self.sp_i_train,
+                self.val_dict,
+                self.test_dict
+            )
+            if self.val_dict:
+                sp_i_val = self._to_bool_sparse(self.val_dict)
+                val_candidate_items = val_neg_samples + sp_i_val
+                self.val_mask = val_candidate_items.toarray()
+            sp_i_test = self._to_bool_sparse(self.test_dict)
+            test_candidate_items = test_neg_samples + sp_i_test
+            self.test_mask = test_candidate_items.toarray()
+
+    def _dataframe_to_dict(self, data):
         # users = list(data['userId'].unique())
 
         "Conversion to Dictionary"
@@ -149,45 +164,27 @@ class DataSet(metaclass=DataSetRequiredAttributesController):
         #     ratings[u] = dict(zip(sel_['itemId'], sel_['rating']))
         return ratings
 
-    def build_dict(self, dataframe):
-        #ratings = dataframe.set_index('userId')[['itemId', 'rating']].apply(lambda x: (x['itemId'], float(x['rating'])), 1)\
-        #    .groupby(level=0).agg(lambda x: dict(x.values)).to_dict()
-        ratings = {
-            user: {item: float(rating) for item, rating in zip(group['itemId'], group['rating'])}
-            for user, group in dataframe.groupby('userId')
-        }
-        # for u in users:
-        #     sel_ = dataframe[dataframe['userId'] == u]
-        #     ratings[u] = dict(zip(sel_['itemId'], sel_['rating']))
-        return ratings
+    def _build_sparse(self, dict, users, items):
 
-    def build_sparse(self):
-
-        rows_cols = [(u, i) for u, items in self.i_train_dict.items() for i in items.keys()]
+        rows_cols = [(u, i) for u, items in dict.items() for i in items.keys()]
         rows = [u for u, _ in rows_cols]
         cols = [i for _, i in rows_cols]
         data = sp.csr_matrix((np.ones_like(rows), (rows, cols)), dtype='float32',
-                             shape=(len(self.users), len(self.items)))
+                             shape=(len(users), len(items)))
         return data
 
-    def build_sparse_ratings(self):
-        rows_cols_ratings = [(u, i, r) for u, items in self.i_train_dict.items() for i, r in items.items()]
+    def _build_sparse_ratings(self, dict, users, items):
+        rows_cols_ratings = [(u, i, r) for u, items in dict.items() for i, r in items.items()]
         rows = [u for u, _, _ in rows_cols_ratings]
         cols = [i for _, i, _ in rows_cols_ratings]
         ratings = [r for _, _, r in rows_cols_ratings]
 
         data = sp.csr_matrix((ratings, (rows, cols)), dtype='float32',
-                             shape=(len(self.users), len(self.items)))
+                             shape=(len(users), len(items)))
 
         return data
 
-    def get_test(self):
-        return self.test_dict
-
-    def get_validation(self):
-        return self.val_dict if hasattr(self, 'val_dict') else None
-
-    def to_bool_sparse(self, test_dict):
+    def _to_bool_sparse(self, test_dict):
         i_test = [(self.public_users[user], self.public_items[i])
                   for user, items in test_dict.items() if user in self.public_users.keys()
                   for i in items.keys() if i in self.public_items.keys()]
@@ -197,15 +194,27 @@ class DataSet(metaclass=DataSetRequiredAttributesController):
                                shape=(len(self.public_users.keys()), len(self.public_items.keys())))
         return i_test
 
-    def align_with_training(self, train, side_information_data):
+    def get_test(self):
+        return self.test_dict
+
+    def get_validation(self):
+        return self.val_dict if hasattr(self, 'val_dict') else None
+
+    def build_items_neighbour(self):
+        row, col = self.sp_i_train.nonzero()
+        edge_index = np.array([row, col])
+        iu_dict = {i: edge_index[0, iu].tolist() for i, iu in
+                   enumerate(list((edge_index[1] == i).nonzero()[0] for i in list(self.private_items.keys())))}
+        return iu_dict
+
+    def _align_with_training(self, side_information_data):
         """Alignment with training"""
 
         def equal(a, b, c):
             return len(a) == len(b) == len(c)
 
-        train_dict = self.dataframe_to_dict(train)
-        users = set(train_dict.keys())
-        items = set({k for a in train_dict.values() for k in a.keys()})
+        users = set(self.train_dict.keys())
+        items = set({k for a in self.train_dict.values() for k in a.keys()})
         users_items = []
         side_objs = []
         for k, v in side_information_data.__dict__.items():
