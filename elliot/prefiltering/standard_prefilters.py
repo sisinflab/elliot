046820254c4e450731771fcbbc109ccbe0b0d179
Francesco Falcone <f.falcone3@studenti.poliba.it>
2025-05-05 10:28:24 +0200

    Refactored PreFilter class

--- elliot/prefiltering/standard_prefilters.py
+++ elliot/prefiltering/standard_prefilters.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 from types import SimpleNamespace
 
@@ -13,224 +12,165 @@ prefiltering:
 
 class PreFilter:
 
+    _mask = None
+
     @staticmethod
     def filter(d: pd.DataFrame, ns: SimpleNamespace) -> pd.DataFrame:
         if not hasattr(ns, "prefiltering"):
             return d
-        ns = ns.prefiltering
         dataframe = d.copy()
-
-        for strategy in ns:
+        for strategy in ns.prefiltering:
             dataframe = PreFilter.single_filter(dataframe, strategy)
-
         return dataframe
 
     @staticmethod
     def single_filter(d: pd.DataFrame, ns: SimpleNamespace) -> pd.DataFrame:
-
         strategy = getattr(ns, "strategy", None)
+        if not strategy:
+            raise ValueError("Missing strategy")
         data = d.copy()
-        if strategy == "global_threshold":
-            threshold = getattr(ns, "threshold", None)
-            if threshold is not None:
-                if str(threshold).isdigit():
-                    data = PreFilter.filter_ratings_by_threshold(data, threshold)
-                elif threshold == "average":
-                    data = PreFilter.filter_ratings_by_global_average(data)
+
+        match strategy:
+            case "global_threshold":
+                threshold = PreFilter._get_validated_attr(ns, "threshold", expected_type=(int, str))
+                if isinstance(threshold, str):
+                    if threshold == "average":
+                        PreFilter._filter_ratings_by_global_average(data)
+                    else:
+                        raise ValueError(f"Invalid threshold value '{threshold}'")
                 else:
-                    raise Exception("Threshold value not recognized")
-            else:
-                raise Exception("Threshold option is missing")
+                    PreFilter._filter_ratings_by_threshold(data, PreFilter._get_validated_attr(ns, "threshold"))
 
-        elif strategy == "user_average":
-            data = PreFilter.filter_ratings_by_user_average(data)
+            case "user_average":
+                PreFilter._filter_ratings_by_user_average(data)
 
-        elif strategy == "user_k_core":
-            core = getattr(ns, "core", None)
-            if core is not None:
-                if str(core).isdigit():
-                    data = PreFilter.filter_users_by_profile_size(data, core)
-                else:
-                    raise Exception("Core option is not a digit")
-            else:
-                raise Exception("Core option is missing")
+            case "user_k_core":
+                PreFilter._filter_users_by_profile_size(data, PreFilter._get_validated_attr(ns, "core"))
 
-        elif strategy == "item_k_core":
-            core = getattr(ns, "core", None)
-            if core is not None:
-                if str(core).isdigit():
-                    data = PreFilter.filter_items_by_popularity(data, core)
-                else:
-                    raise Exception("Core option is not a digit")
-            else:
-                raise Exception("Core option is missing")
+            case "item_k_core":
+                PreFilter._filter_items_by_popularity(data, PreFilter._get_validated_attr(ns, "core"))
 
-        elif strategy == "iterative_k_core":
-            core = getattr(ns, "core", None)
-            if core is not None:
-                if str(core).isdigit():
-                    data = PreFilter.filter_iterative_k_core(data, core)
-                else:
-                    raise Exception("Core option is not a digit")
-            else:
-                raise Exception("Core option is missing")
+            case "iterative_k_core":
+                return PreFilter._filter_iterative_k_core(data, PreFilter._get_validated_attr(ns, "core"))
 
-        elif strategy == "n_rounds_k_core":
-            core = getattr(ns, "core", None)
-            n_rounds = getattr(ns, "rounds", None)
-            if (core is not None) and (n_rounds is not None):
-                if str(core).isdigit() and str(n_rounds).isdigit():
-                    data = PreFilter.filter_rounds_k_core(data, core, n_rounds)
-                else:
-                    raise Exception("Core or rounds options are not digits")
-            else:
-                raise Exception("Core or rounds options are missing")
+            case "n_rounds_k_core":
+                core = PreFilter._get_validated_attr(ns, "core")
+                rounds = PreFilter._get_validated_attr(ns, "rounds")
+                return PreFilter._filter_rounds_k_core(data, core, rounds)
 
-        elif strategy == "cold_users":
-            threshold = getattr(ns, "threshold", None)
-            if threshold is not None:
-                if str(threshold).isdigit():
-                    data = PreFilter.filter_retain_cold_users(data, threshold)
-                else:
-                    raise Exception("Threshold option is not a digit")
-            else:
-                raise Exception("Threshold option is missing")
+            case "cold_users":
+                PreFilter._filter_retain_cold_users(data, PreFilter._get_validated_attr(ns, "threshold"))
 
-        else:
-            raise Exception("Misssing strategy")
+            case _:
+                raise ValueError(f"Strategy '{strategy}' not recognized")
 
-        return data
+        return data[PreFilter._mask]
 
     @staticmethod
-    def filter_ratings_by_global_average(d: pd.DataFrame) -> pd.DataFrame:
+    def _get_validated_attr(ns, attr, expected_type=int, required=True):
+        val = getattr(ns, attr, None)
+        if required and val is None:
+            raise ValueError(f"{attr} option is missing")
+
+        if isinstance(expected_type, tuple):
+            msg = f"{attr} must be an integer or a string"
+        else:
+            msg = f"{attr} must be an integer"
+        if not isinstance(val, expected_type):
+            raise ValueError(msg)
+
+        return val
+
+    @staticmethod
+    def _filter_ratings_by_global_average(d: pd.DataFrame) -> None:
         data = d.copy()
         threshold = data["rating"].mean()
+        PreFilter._mask = data['rating'] >= threshold
         print("\nPrefiltering with Global Average")
         print(f"The rating average is {round(threshold, 1)}")
-        print(f"The transactions above threshold are {data[data['rating'] >= threshold]['rating'].count()}")
-        print(f"The transactions below threshold are {data[data['rating'] < threshold]['rating'].count()}")
-        return data[data["rating"] >= threshold]
+        print(f"The transactions above threshold are {PreFilter._mask.sum()}")
+        print(f"The transactions below threshold are {(~PreFilter._mask).sum()}")
 
     @staticmethod
-    def filter_ratings_by_threshold(d: pd.DataFrame, threshold) -> pd.DataFrame:
+    def _filter_ratings_by_threshold(d: pd.DataFrame, threshold) -> None:
         data = d.copy()
+        PreFilter._mask = data['rating'] >= threshold
         print("\nPrefiltering with fixed threshold")
         print(f"The rating threshold is {round(threshold, 1)}")
-        print(f"The transactions above threshold are {data[data['rating'] >= threshold]['rating'].count()}")
-        print(f"The transactions below threshold are {data[data['rating'] < threshold]['rating'].count()}\n")
-        return data[data["rating"] >= threshold]
+        print(f"The transactions above threshold are {PreFilter._mask.sum()}")
+        print(f"The transactions below threshold are {(~PreFilter._mask).sum()}\n")
 
     @staticmethod
-    def filter_ratings_by_user_average(d: pd.DataFrame) -> pd.DataFrame:
+    def _filter_ratings_by_user_average(d: pd.DataFrame) -> None:
         data = d.copy()
-        user_groups = data.groupby(['userId'])
-        for name, group in user_groups:
-            threshold = group["rating"].mean()
-            data.loc[group.index, 'accept_flag'] = data.loc[group.index, 'rating'] >= threshold
-
+        PreFilter._mask = data['rating'] >= data.groupby('userId')['rating'].transform('mean')
         print("\nPrefiltering with user average")
-        print(f"The transactions above threshold are {data[data['accept_flag']]['rating'].count()}")
-        print(f"The transactions below threshold are {data[data['accept_flag'] == False]['rating'].count()}\n")
-        return data[data["accept_flag"] == True].drop(columns=["accept_flag"]).reset_index(drop=True)
+        print(f"The transactions above threshold are {PreFilter._mask.sum()}")
+        print(f"The transactions below threshold are {(~PreFilter._mask).sum()}\n")
 
     @staticmethod
-    def filter_users_by_profile_size(d: pd.DataFrame, threshold) -> pd.DataFrame:
+    def _filter_users_by_profile_size(d: pd.DataFrame, threshold) -> None:
         data = d.copy()
+        user_counts = data["userId"].value_counts()
+        valid_users = user_counts[user_counts >= threshold].index
+        PreFilter._mask = data["userId"].isin(valid_users)
         print(f"\nPrefiltering with user {threshold}-core")
         print(f"The transactions before filtering are {len(data)}")
         print(f"The users before filtering are {data['userId'].nunique()}")
-        user_groups = data.groupby(['userId'])
-        data = user_groups.filter(lambda x: len(x) >= threshold)
-        print(f"The transactions after filtering are {len(data)}")
-        print(f"The users after filtering are {data['userId'].nunique()}")
-        return data
+        print(f"The transactions after filtering are {PreFilter._mask.sum()}")
+        print(f"The users after filtering are {data[PreFilter._mask]['userId'].nunique()}")
 
     @staticmethod
-    def filter_items_by_popularity(d: pd.DataFrame, threshold) -> pd.DataFrame:
+    def _filter_items_by_popularity(d: pd.DataFrame, threshold) -> None:
         data = d.copy()
+        item_counts = data["itemId"].value_counts()
+        valid_items = item_counts[item_counts >= threshold].index
+        PreFilter._mask = data["itemId"].isin(valid_items)
         print(f"\nPrefiltering with item {threshold}-core")
         print(f"The transactions before filtering are {len(data)}")
         print(f"The items before filtering are {data['itemId'].nunique()}")
-        item_groups = data.groupby(['itemId'])
-        data = item_groups.filter(lambda x: len(x) >= threshold)
-        print(f"The transactions after filtering are {len(data)}")
-        print(f"The items after filtering are {data['itemId'].nunique()}")
-        return data
+        print(f"The transactions after filtering are {PreFilter._mask.sum()}")
+        print(f"The items after filtering are {data[PreFilter._mask]['itemId'].nunique()}")
 
     @staticmethod
-    def filter_iterative_k_core(d: pd.DataFrame, threshold) -> pd.DataFrame:
+    def _filter_iterative_k_core(d: pd.DataFrame, threshold) -> pd.DataFrame:
         data = d.copy()
-        check_var = True
-        original_length = len(data)
+        original_length = -1
         print("\n**************************************")
         print(f"Iterative {threshold}-core")
-        while check_var:
-            data = PreFilter.filter_users_by_profile_size(data, threshold)
-            data = PreFilter.filter_items_by_popularity(data, threshold)
-            new_length = len(data)
-            if original_length == new_length:
-                check_var = False
-            else:
-                original_length = new_length
+        while original_length != len(data):
+            original_length = len(data)
+            PreFilter._filter_users_by_profile_size(data, threshold)
+            data = data[PreFilter._mask]
+            PreFilter._filter_items_by_popularity(data, threshold)
+            data = data[PreFilter._mask]
         print("**************************************\n")
 
         return data
 
     @staticmethod
-    def filter_rounds_k_core(d: pd.DataFrame, threshold, n_rounds) -> pd.DataFrame:
+    def _filter_rounds_k_core(d: pd.DataFrame, threshold, n_rounds) -> pd.DataFrame:
         data = d.copy()
         print("\n**************************************")
         print(f"{n_rounds} rounds of user/item {threshold}-core")
         for i in range(n_rounds):
             print(f"Iteration:\t{i}")
-            data = PreFilter.filter_users_by_profile_size(data, threshold)
-            data = PreFilter.filter_items_by_popularity(data, threshold)
+            PreFilter._filter_users_by_profile_size(data, threshold)
+            data = data[PreFilter._mask]
+            PreFilter._filter_items_by_popularity(data, threshold)
+            data = data[PreFilter._mask]
         print("**************************************\n")
 
         return data
 
     @staticmethod
-    def filter_retain_cold_users(d: pd.DataFrame, threshold) -> pd.DataFrame:
+    def _filter_retain_cold_users(d: pd.DataFrame, threshold) -> None:
         data = d.copy()
+        user_counts = data["userId"].value_counts()
+        cold_users = user_counts[user_counts <= threshold].index
+        PreFilter._mask = data["userId"].isin(cold_users)
         print(f"\nPrefiltering retaining cold users with {threshold} or less ratings")
         print(f"The transactions before filtering are {len(data)}")
         print(f"The users before filtering are {data['userId'].nunique()}")
-        user_groups = data.groupby(['userId'])
-        data = user_groups.filter(lambda x: len(x) <= threshold)
-        print(f"The transactions after filtering are {len(data)}")
-        print(f"The users after filtering are {data['userId'].nunique()}")
-        return data
-
-
-# import unittest
-#
-#
-#
-# class NameSpaceModelTest(unittest.TestCase):
-#     def setUp(self):
-#         self.column_names = ['userId', 'itemId', 'rating']
-#         self.data = pd.read_csv("../../data/example/trainingset.tsv", sep="\t", header=None, names=self.column_names)
-#
-#     def test_global_average(self):
-#         data = PreFilter.filter_ratings_by_global_average(self.data)
-#
-#     def test_threshold(self):
-#         data = PreFilter.filter_ratings_by_threshold(self.data, 3)
-#
-#     def test_user_average(self):
-#         data = PreFilter.filter_ratings_by_user_average(self.data)
-#
-#     def test_user_k_core(self):
-#         data = PreFilter.filter_users_by_profile_size(self.data, 10)
-#
-#     def test_item_k_core(self):
-#         data = PreFilter.filter_items_by_popularity(self.data, 10)
-#
-#     def test_iterative_k_core(self):
-#         data = PreFilter.filter_iterative_k_core(self.data, 10)
-#
-#     def test_rounds_k_core(self):
-#         data = PreFilter.filter_rounds_k_core(self.data, 10, 2)
-#
-#     def test_cold_users(self):
-#         data = PreFilter.filter_retain_cold_users(self.data, 10)
+        print(f"The transactions after filtering are {PreFilter._mask.sum()}")
+        print(f"The users after filtering are {data[PreFilter._mask]['userId'].nunique()}")
