ef7f1d5dd7ca75e633977a06cc7b4159ea01f42e
Francesco Falcone <f.falcone3@studenti.poliba.it>
2025-05-28 20:30:49 +0200

    Refactored tests for dataset loading strategies

--- tests/data/test_dataset_loader.py
+++ tests/data/test_dataset_loader.py
@@ -1,12 +1,9 @@
 import importlib
-from pathlib import Path
-from tests.utils import *
+from tests.test_utils import *
+from tests.test_params import params_dataset_loader
 
 import pytest
 from unittest.mock import patch
-
-current_path = Path(__file__).resolve().parent
-data_path = Path.cwd().parent.parent / 'data'
 
 
 def dataloader(config_dict):
@@ -26,22 +23,7 @@ def dataloader(config_dict):
 
 class TestDataSetLoader:
 
-    @pytest.mark.parametrize(
-        "params",
-        [
-            {
-                "folder_path": str(current_path / "fixed_strategy_with_validation"),
-                "train_shape": 40,
-                "val_shape": 5,
-                "test_shape": 5,
-            },
-            {
-                "folder_path": str(current_path / "fixed_strategy"),
-                "train_shape": 45,
-                "test_shape": 5,
-            },
-        ],
-    )
+    @pytest.mark.parametrize('params', params_dataset_loader['fixed_strategy'])
     @time_single_test
     def test_fixed_strategy(self, params):
         val = True if 'val_shape' in params.keys() else False
@@ -67,16 +49,7 @@ class TestDataSetLoader:
         else:
             check_dataloader(loader.tuple_list[0][0], params['train_shape'])
 
-    @pytest.mark.parametrize(
-        "params", [
-            {
-                'root_folder': str(current_path / 'hierarchy_strategy'),
-                "train_shapes": [[40, 40], 45],
-                "val_shapes": [[5, 5]],
-                "test_shapes": [5, 5],
-            }
-        ]
-    )
+    @pytest.mark.parametrize('params', params_dataset_loader['hierarchy_strategy'])
     def test_hierarchy_strategy(self, params):
         config = {
             'strategy': 'hierarchy',
@@ -98,25 +71,7 @@ class TestDataSetLoader:
                 assert t[0].shape[0] == params['train_shapes'][i]
             i += 1
 
-    @pytest.mark.parametrize(
-        "params", [
-            {
-                'dataset_folder': str(current_path / 'dataset_strategy'),
-                'test_ratio': 0.2,
-                'df_shape': 50
-            },
-            #{
-            #    'dataset_folder': str(data_path / 'cat_dbpedia_movielens_1m_v030'),
-            #    'test_ratio': 0.2,
-            #    'df_shape': 1000209
-            #}
-            #{
-            #    'dataset_folder': str(data_path / 'cat_dbpedia_movielens_10m'),
-            #    'test_ratio': 0.2,
-            #    'df_shape': 10000054
-            #}
-        ]
-    )
+    @pytest.mark.parametrize('params', params_dataset_loader['dataset_strategy'])
     @time_single_test
     def test_dataset_strategy(self, params):
         dataset_path = params['dataset_folder'] + '/dataset.tsv'
@@ -136,7 +91,6 @@ class TestDataSetLoader:
                 return_value=[(train_df, test_df)]
             ) as mock_splitter
         ):
-
             loader = dataloader(config)
 
             mock_splitter.assert_called_once()
@@ -145,58 +99,80 @@ class TestDataSetLoader:
             assert loader.tuple_list[0][0].equals(train_df)
             assert loader.tuple_list[0][1].equals(test_df)
 
+    @pytest.mark.parametrize('params', params_dataset_loader['filter_nan'])
+    def test_filter_nan(self, params):
+        dataset_path = params['dataset_folder'] + '/dataset.tsv'
+        config = {
+            'strategy': 'dataset',
+            'dataset_path': dataset_path
+        }
+
+        df_mock = read_dataset(dataset_path)
+
+        with (
+            patch(
+                "elliot.dataset.loader_coordinator.Splitter.process_splitting",
+                return_value=[(df_mock, df_mock)]
+            )
+        ):
+            loader = dataloader(config)
+
+            assert loader.dataframe.shape[0] == params['df_final_shape']
+            assert loader.dataframe.duplicated().sum() == 0
+            assert not loader.dataframe["timestamp"].isna().any()
+
 
 class TestDataSetLoaderFailures:
 
-    @pytest.mark.parametrize('val', [True, False])
-    def test_fixed_strategy_missing_train_path(self, val):
-        folder_path = current_path / (
-            'fixed_strategy' if not val else 'fixed_strategy_with_validation'
-        )
+    @pytest.mark.parametrize('params', params_dataset_loader['fixed_strategy'])
+    @pytest.mark.xfail(raises=AttributeError)
+    def test_fixed_strategy_missing_train_path(self, params):
+        val = True if 'val_shape' in params.keys() else False
 
         config = {
             'strategy': 'fixed',
-            'test_path': str(current_path / 'test.tsv'),
-            **({'validation_path': str(folder_path / 'val.tsv')} if val else {})
+            'test_path': params['folder_path'] + '/test.tsv',
+            **({'validation_path': params['folder_path'] + '/val.tsv'} if val else {})
         }
 
-        with pytest.raises(AttributeError):
-            dataloader(config)
+        dataloader(config)
 
-    @pytest.mark.parametrize('val', [True, False])
-    def test_fixed_strategy_missing_file(self, val):
-        folder_path = current_path / 'fixed_strategy_missing_file'
+    @pytest.mark.parametrize('params', params_dataset_loader['fixed_strategy_missing_file'])
+    @pytest.mark.xfail(raises=FileNotFoundError)
+    def test_fixed_strategy_missing_file(self, params):
+        val = True if 'val_shape' in params.keys() else False
 
         config = {
             'strategy': 'fixed',
-            'train_path': str(folder_path / 'train.tsv'),
-            'test_path': str(folder_path / 'test.tsv'),
-            **({'validation_path': str(folder_path / 'val.tsv')} if val else {})
+            'train_path': params['folder_path'] + '/train.tsv',
+            'test_path': params['folder_path'] + '/test.tsv',
+            **({'validation_path': params['folder_path'] + '/val.tsv'} if val else {})
         }
 
-        with pytest.raises(FileNotFoundError):
-            dataloader(config)
+        dataloader(config)
 
+    @pytest.mark.xfail(raises=FileNotFoundError)
     def test_hierarchy_strategy_missing_root_folder(self):
         config = {
             'strategy': 'hierarchy',
             'root_folder': 'non/existent/path'
         }
 
-        with pytest.raises(FileNotFoundError):
-            dataloader(config)
+        dataloader(config)
 
+    @pytest.mark.xfail(raises=FileNotFoundError)
     def test_dataset_strategy_missing_dataset(self):
         config = {
             'strategy': 'dataset',
             'dataset_path': 'nonexistent/file.tsv'
         }
 
-        with pytest.raises(FileNotFoundError):
-            dataloader(config)
+        dataloader(config)
 
-    def test_dataset_strategy_invalid_split(self, monkeypatch):
-        dataset_path = str(current_path / 'dataset_strategy/dataset.tsv')
+    @pytest.mark.parametrize('params', params_dataset_loader['dataset_strategy'])
+    @pytest.mark.xfail(raises=IndexError)
+    def test_dataset_strategy_invalid_split(self, params, monkeypatch):
+        dataset_path = params['dataset_folder'] + '/dataset.tsv'
 
         def fake_splitter(*args, **kwargs):
             return [] # Failed split
@@ -211,36 +187,7 @@ class TestDataSetLoaderFailures:
             'dataset_path': dataset_path
         }
 
-        with pytest.raises(IndexError):
-            dataloader(config)
-
-
-def apply_cleaning(df, users, items):
-    cleaner_class = getattr(importlib.import_module("elliot.dataset"), 'Cleaner')
-    cleaner = cleaner_class(df, users, items)
-    return cleaner.process_cleaning()
-
-
-class TestCleaner:
-    @pytest.mark.parametrize(
-        "params", [
-            {
-                'dataset_folder': str(current_path / 'filter_nan'),
-                'users': [1],
-                'items': [1],
-                'df_final_shape': 2
-            }
-        ]
-    )
-    def test_filter_nan(self, params):
-        dataset_path = params['dataset_folder'] + '/dataset.tsv'
-        df_mock = read_dataset(dataset_path)
-
-        cleaned_df = apply_cleaning(df_mock, set(params['users']), set(params['items']))
-
-        assert cleaned_df.shape[0] == params['df_final_shape']
-        assert cleaned_df.duplicated().sum() == 0
-        assert not cleaned_df["timestamp"].isna().any()
+        dataloader(config)
 
 
 if __name__ == '__main__':
